6c53637158f06099486df0a18419e3d21fe33058
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-01-17 02:32:09 +0900

    Fix HTML template indentation

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
-  <section class="hero is-medium is-dark is-fullheight">
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <pre class="logo"><code v-html="hjs"></code></pre>
-        <h1 class="title">h.js</h1>
-        <p class="subtitle"><strong>2KB</strong> JavaScript Syntax Highlighter</p>
-      </div>
-    </div>
-
-    <div class="hero-foot">
-      <nav class="tabs is-boxed is-fullwidth">
-        <div class="container">
-          <ul>
-            <li v-bind:class="{'is-active': $route.path === '/'}"><nuxt-link to="/">README</nuxt-link></li>
-            <li v-bind:class="{'is-active': $route.path === '/demo'}"><nuxt-link to="/demo">Demonstration</nuxt-link></li>
-          </ul>
+    <section class="hero is-medium is-dark is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <pre class="logo"><code v-html="hjs"></code></pre>
+          <h1 class="title">h.js</h1>
+          <p class="subtitle"><strong>2KB</strong> JavaScript Syntax Highlighter</p>
         </div>
-      </nav>
-    </div>
-  </section>
+      </div>
+
+      <div class="hero-foot">
+        <nav class="tabs is-boxed is-fullwidth">
+          <div class="container">
+            <ul>
+              <li v-bind:class="{'is-active': $route.path === '/'}"><nuxt-link to="/">README</nuxt-link></li>
+              <li v-bind:class="{'is-active': $route.path === '/demo'}"><nuxt-link to="/demo">Demonstration</nuxt-link></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </section>
 
     <nuxt-child></nuxt-child>
   </div>
