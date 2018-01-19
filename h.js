f86125676c2cacab762e9e2e2e0bbf6bf43a6de7
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-01-19 23:42:13 +0900

    Update h.js

--- h.js
+++ h.js
@@ -1,28 +1,28 @@
-(([p],c,s)=>{s=`(fz{q(Z)=YfW'VZ.amd?Z([],f):qUoduT)=Yobject'VmoduT.expo
-rts?UoduT.exportSf(R:(FW('return(this)')().h=f(R})(hz(h=(s,f,t,e,c,r,m,
-l,p)z{tY';p=[];e=tztQ[<&]/g,czP&#Mc.charCodeAt()};P);c=_z(_=sQ^Ls*)L/L/
-.*|K*([^]*?K*K/|[^]*R)/,(_,w,m)z(t+=PMw}JSc>MeU)Q@Kw+/g,dzPJSd>MdHP)HP,
-''R)==s?(S_):c(S_);(r=_z(Sc(s)Q^Ls*)L/[^*](L[LK.|.)*?K]|KK.|.)+?K/[imgu
-ys]*|.*R/,(_,w,m)z(t+=PMw}JSr>MmHP,''RR();whiT(U=/^([^]*?)Lb(asyncG=[Kx
-20Kt]*DfWKb|DKw+|K(.*?KR[Kx20Kt]*zR|break|catch|class|conDst|tinue)|def
-ault|do|else|finally|for|from|fW|if|import|Tt|static|super|switch|this|
-try|var|whiT|with|(await|case|deTte|export|extends|inDstanceof)?|new|of
-|return|throw|q|void|yieldRKb|LbD0[xX][a-fA-FKd]+|true|false|null|unZd|
-NaN|Infinity)Kb|DK.Kd+|KbD0[oObB9?Kd+DK.Kd*)?)D[eE][-+]?Kd+)?)|(Lx22|')
-DDKK[^]|.)*?K7|.*R|L.{3}|[L[{;,?:]|z)|([.)K]}9|(--|[+][+]|V|[|][|]|D<<|
->>>?|[-+K/%&|^<>]|K*K*?)=?|~|[!=]=?=?)|([\$Kw]+G=Ks*[(P]R|(P)|(.G=K/[*K
-/]R)/.exec(c(sR)     V810-1]!Y}'||f!=1RS685),m[1   2]?!(n         z{t+=
-e819+'JSs>P';whi     T((n=/^([^]*?)LK.|(P)|LMR/.   exec    (sRV!n    [1
-+29S6(n[5),t+=e(     n[19+(n[2+2]?'JSp>KM4JSi>'+   (l=    h(s,1),    q(
-l)>'r'?(sY',l+'4     '):(Sl[1],l[0]+'4JSp>H'R:e(   n[2    ]R;t+=(n?e(n[
-0],S6(n[5R:e(s,s     Y'R+'4'})():(t+=e819+812+1]   ||m[   1+2]V(p[2]=Y.
-'V/^Ks*\$/.test8            19V(/^Ks*[(P]/.test(   s,m[1+         2]=m[
-2+2]Y')?!811]=m[             29:1)||/^Ks*:/.test   (sR?e8         29:'J
-sY+'___k_vs_ppof     '[m.i   ndexOf82]    ,2+1)]   +'>'+e829+'4')   ,p=
-m),(fV810-1]=Y}'     Vf--|   |m[7+1]=Y    {'Vf++   R,82+2]||m[7+1    ]|
-|m[109Vr();retur     nU?[t   +e8   19,    685)]:   t+e    (sR}Rz=    >q
-typeofZdefineY='     Wunct   ion   V&&             U(m    TleSs=    R))
-Q.replace(/P\`M$     {L(KK   \\J   <spa           nKx20cl         asH}4
-G(?DG:9])8U[6s.slice50].Tngth4</span>`.replace(/\s/g,'');for((c)in(p))s
-=(c=s.split(p[c])).join(c.pop());eval(s)})`45689DGHJKLMPQRSTUVWYZqz`/**
-**********************/// (C) 2015-2018 TSUYUSATO "MakeNowJust" Kitsune
+(([p],s=[...p].reduce((s,c)=>(c=s.split(c)).join(c.pop()),`(fzq(Z)=YfW'
+VZ.amd?Z([],f):qUoduT)=Yobject'VmoduT.exports?UoduT.exportSf(R:(FW('ret
+urn(this)')().h=f(R)(hz(h=(s,f,t,e,c,r,m,l,p)z{tY';p=[];e=tztQ[<&]/g,cz
+P&#Mc.charCodeAt()};P);c=_z(_=sQ^Ls*)L/L/.*|K*([^]*?K*K/|[^]*R)/,(_,w,m
+)z(t+=PMw}JSc>MeU)Q@Kw+/g,dzPJSd>MdHP)HP,''R)==s?(S_):c(S_);(r=_z(Sc(s)
+Q^Ls*)L/[^*](L[LK.|.)*?K]|KK.|.)+?K/[imguys]*|.*R/,(_,w,m)z(t+=PMw}JSr>
+MmHP,''RR();whiT(U=/^([^]*?)Lb(asyncG=[Kx20Kt]*DfWKb|DKw+|K(.*?KR[Kx20K
+t]*zR|break|catch|class|conDst|tinue)|default|do|else|finally|for|from|
+fW|if|import|Tt|static|super|switch|this|try|var|whiT|with|(await|case|
+deTte|export|extends|inDstanceof)?|new|of|return|throw|q|void|yieldRKb|
+LbD0[xX][a-fA-FKd]+|true|false|null|unZd|NaN|Infinity)Kb|DK.Kd+|KbD0[oO
+bB9?Kd+DK.Kd*)?)D[eE][-+]?Kd+)?)|(Lx22|')DDKK[^]|.)*?K7|.*R|L.{3}|[L[{;
+,?:]|z)|([.)K]}9|(--|[+][+]|V|[|][|]|D<<|>>>?|[-+K/%&|^<>]|K*K*?)=?|~|[
+!=]=?=?)|([$Kw]+     G=Ks*[(P]R|(P)|(.G=K/[*K/]R   )/.exe         c(c(s
+R)V810-1]!Y}'||f     !=1RS685),m[12]?!(nz{t+=e81   9+'J    Ss>P';    wh
+iT((n=/^([^]*?)L     K.|(P)|LMR/.exec(sRV!n[1+29   S6(    n[5),t+    =e
+(n[19+(n[2+2]?'J     Sp>KM4JSi>'+(l=h(s,1),q(l)>   'r'    ?(sY',l+'4'):
+(Sl[1],l[0]+'4JS     p>H'R:e(n[2]R;t+=(n?e(n[0],   S6(n   [5R:e(s,sY'R+
+'4'})():(t+=e819            +812+1]||m[1+2]V(p[2   ]=Y.'V         /^Ks*
+$/.test819V(/^Ks             *[(P]/.test(s,m[1+2   ]=m[2+         2]Y')
+?!811]=m[29:1)||     /^Ks*   :/.test(s    R?e829   :'JsY+'___k_vs   _pp
+of'[m.indexOf82]     ,2+1)   ]+'>'+e82    9+'4')   ,p=m),(fV810-1    ]=
+Y}'Vf--||m[7+1]=     Y{'Vf   ++R   ,82    +2]||m   [7+    1]||m[1    09
+Vr();returnU?[t+     e819,   685   )]:             t+e    (sR}Rz    =>q
+typeofZdefineY='     Wunct   ion   V&&U           (mTleSs         =R))Q
+.replace(/P\`M\${L(KK\\J<spanKx20clasH}4G(?DG:9])8U[6s.slice50].Tngth4<
+/span>`.replace(/\s/g,'')))=>eval(s))`45689DGHJKLMPQRSTUVWYZqz`/*////*/
+/*********** (C) 2015-2018 TSUYUSATO "MakeNowJust" Kitsune ***********/
