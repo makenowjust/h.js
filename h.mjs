d4489783c0d6bfabdeac12d348be7f2e988dc806
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-01-23 00:16:58 +0900

    Don't use tagged template literal for uglify bug

--- h.mjs
+++ h.mjs
@@ -1,26 +1,26 @@
-export default (([p],s=[...p].reduce((s,c)=>(c=s.split(c)).join(c.pop()
-),`(hzh=q,f,t='',e=tztj[<&]/g,czZ&#Yc.charCodeAt()};ZWc=_z(_=sj^Vs*)V/V
-/.*|U*([^]*?U*U/|[^]*T)/,(_,w,m)z(t+=ZYw}Sc>YRm)j@Uw+/g,dzZSd>YdQZ)QZ,'
-'T)==s?q=_):cq=_Wr=_zq=cq)j^Vs*)V/[^*](V[VU.|.)*?U]|UU.|.)+?U/[imguys]*
-|.*T/,(_,w,m)z(t+=ZYw}Sr>YmQZ,''TWm=r(Wl,p=[Pz{whilR(m=/^([^]*?)Vb(asyn
-cM=[Ux20Ut]*LfunctionUb|LUw+|U(.*?UT[Ux20Ut]*zT|break|catch|class|conLs
-t|tinue)|default|do|else|finally|for|from|function|if|import|let|static
-|super|switch|this|try|var|while|with|(await|case|delete|export|extends
-|inLstanceof)?|new|of|return|throw|typeof|void|yieldTUb|VbL0[xX][a-fA-F
-Ud]+|true|false|null|undefined|NaN|Infinity)Ub|LU.Ud+|UbL0[oObBP?Ud+LU.
-Ud*)?)L[eE][-+]?Ud+)?)|(Vx22|')LLUU[^]|.)*?U7|.*T|V.{3}|[V[{;,?:]|z)|([
-.)U]}P|(--|[+][+]|&&|[|][|]|L<<|>>>?|[-+U/%&|^<>]|U*U*?)=?|~|[!=]=?=?)|
-([$Uw]+M=Us*[(Z]     T|(Z)|(.M=U/[*U/]T)/.exec(c   qT)&&K         10-1]
-!='}'||f!=1Ts=JH     GWH12]?!(nz{t+=RH1P+'Ss>Z';   whil    R(n=/^    ([
-^]*?)VU.|(Z)|VYT     /.execqT&&!n[1+2Ps=Jn[GWt+=   Rn[    1P+(n[2    +2
-]?'Sp>UYDSi>'+(l     =hq,1Wtypeof(l)>'r'?q='',l+   'D'    ):q=l[1],l[0]
-+'DSp>Q'T:Rn[2]T     ;t+=(n?Rn[0],s=Jn[GT:eq,s='   'T+'    D'})():(t+=R
-H1P+K12+1]||H1+2            ]&&(p[2]=='.'&&/^Us*   $/.tes     tK1P&&(/^
-Us*[(Z]/.testq,H             1+2]=H2+2]='')?!K11   ]=H2P:1)       ||/^U
-s*:/.testqT?RH2P     :ZSY'   ___k_vs_p    pof'[m   .indexOfK2],     1+2
-)]}>YRH2PQZWp=mW     (f&&K   10-1]=='}    '&&f--   ||H7+1]=='{'&&    f+
-+)WK2+2]||H7+1]|     |H10P   &&r   ();    return   (m?    [t+RH1]    WJ
-HG)]:t+eqT})()z=     >q(sj   .re   pla             cR/    Z\`Y\$    {W)
-,V(UU\\T))S<span     Ux20c   las   s=Re           (Q}DP])         M(?LM
-:K(HJs.slicRHm[G0].lengthD</span>`.replace(/\s/g,'')))=>eval(s))//QUINE
-`DGHJKLMPQRSTUVWYZjqz`//! (C) 2015-2018 TSUYUSATO "MakeNowJust" Kitsune
+export default ((p,s=[...p].reduce((s,c)=>(c=s.split(c)).join(c.pop()),
+`(hzh=q,f,t='',e=tztj[<&]/g,czZ&#Yc.charCodeAt()};ZWc=_z(_=sj^Vs*)V/V/.
+*|U*([^]*?U*U/|[^]*T)/,(_,w,m)z(t+=ZYw}Sc>YRm)j@Uw+/g,dzZSd>YdQZ)QZ,''T
+)==s?q=_):cq=_Wr=_zq=cq)j^Vs*)V/[^*](V[VU.|.)*?U]|UU.|.)+?U/[imguys]*|.
+*T/,(_,w,m)z(t+=ZYw}Sr>YmQZ,''TWm=r(Wl,p=[Pz{whilR(m=/^([^]*?)Vb(asyncM
+=[Ux20Ut]*LfunctionUb|LUw+|U(.*?UT[Ux20Ut]*zT|break|catch|class|conLst|
+tinue)|default|do|else|finally|for|from|function|if|import|let|static|s
+uper|switch|this|try|var|while|with|(await|case|delete|export|extends|i
+nLstanceof)?|new|of|return|throw|typeof|void|yieldTUb|VbL0[xX][a-fA-FUd
+]+|true|false|null|undefined|NaN|Infinity)Ub|LU.Ud+|UbL0[oObBP?Ud+LU.Ud
+*)?)L[eE][-+]?Ud+)?)|(Vx22|')LLUU[^]|.)*?U7|.*T|V.U.U.|[V[{;,?:]|z)|([.
+)U]}P|(--|[+][+]|&&|[|][|]|L<<|>>>?|[-+U/%&|^<>]|U*U*?)=?|~|[!=]=?=?)|(
+[$Uw]+M=Us*[(Z]T     |(Z)|(.M=U/[*U/]T)/.exec(cq   T)&&K1         0-1]!
+='}'||f!=1Ts=JHG     WH12]?!(nz{t+=RH1P+'Ss>Z';w   hilR    (n=/^(    [^
+]*?)VU.|(Z)|VYT/     .execqT&&!n[1+2Ps=Jn[GWt+=R   n[1    P+(n[2+    2]
+?'Sp>UYDSi>'+(l=     hq,1Wtypeof(l)>'r'?q='',l+'   D')    :q=l[1],l[0]+
+'DSp>Q'T:Rn[2]T;     t+=(n?Rn[0],s=Jn[GT:eq,s=''   T+'D    '})():(t+=RH
+1P+K12+1]||H1+2]            &&(p[2]=='.'&&/^Us*$   /.test     K1P&&(/^U
+s*[(Z]/.testq,H1             +2]=H2+2]='')?!K11]   =H2P:1)|       |/^Us
+*:/.testqT?RH2P:     ZSY'_   __k_vs_pp    of'[m.   indexOfK2],1     +2)
+]}>YRH2PQZWp=mW(     f&&K1   0-1]=='}'    &&f--|   |H7+1]=='{'&&f    ++
+)WK2+2]||H7+1]||     H10P&   &r(   );r    eturn(   m?[    t+RH1]W    JH
+G)]:t+eqT})()z=>     q(sj.   rep   lac             R/Z    \`Y\${    W),
+V(UU\\T))S<spanU     x20cl   ass   =Re(           Q}DP])M         (?LM:
+K(HJs.slicRHm[G0].lengthD</span>`.replace(/\s/g,'')))=>eval(s))(`DGHJ${
+'' /*(C) 2015-2018 TSUYUSATO "MakeNowJust" Kitsune*/}KLMPQRSTUVWYZjqz`)
