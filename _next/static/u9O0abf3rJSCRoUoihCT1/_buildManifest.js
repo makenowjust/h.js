bdcf97508d525f5dfbcb8a42a206047aee2b30ec
makenowjust <makenowjust@users.noreply.github.com>
2023-01-23 02:38:55 +0000

    deploy: 43216aa7ee171fa4afcbd073d8a828cbb7fad51a

---
+++ _next/static/u9O0abf3rJSCRoUoihCT1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2d88b23d4e942366.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
